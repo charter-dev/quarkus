082453eef17703b6cc0ced101ab285807942d549
charter43 <charteremail@gmail.com>
2026-03-13 18:47:52 +0700

    add product

---
+++ src/main/java/erp_system/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package erp_system.repository;
+
+import erp_system.entity.master.Product;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProductRepository implements PanacheRepository<Product> {
+}
